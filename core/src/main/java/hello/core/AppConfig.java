422f15c15f6f4e2db3bb4d97b486b218fb1fe021
wono <43237378+WonhoLee97@users.noreply.github.com>
2022-10-05 22:47:09 +0900

    Learning_Spring

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,20 +1,38 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemeberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-public class AppConfig {
+@Configuration
+public class AppConfig { //역할과 구현 클래스가 한 눈에 들어옴. 어플리케이션의 전체 구조 빠르게 파악 가능
 
+    @Bean //Spring 컨테이너에 등록
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemeberRepository()); //생성자 주입
+        return new MemberServiceImpl(memberRepository()); //생성자 주입
     }
 
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemeberRepository();
+    }
+
+    @Bean
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemeberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy(){
+        return new RateDiscountPolicy();//할인정책을 바꾸려면 여기만 변경하면 됨
     }
 
 
