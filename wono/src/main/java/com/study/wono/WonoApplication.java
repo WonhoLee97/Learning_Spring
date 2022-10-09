304def9bc43243da032a7af93c51b22f6540f2d7
wono <43237378+WonhoLee97@users.noreply.github.com>
2022-10-10 00:37:42 +0900

    Learning_Spring

---
+++ wono/src/main/java/com/study/wono/WonoApplication.java
@@ -0,0 +1,13 @@
+package com.study.wono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WonoApplication.class, args);
+	}
+
+}
